9014080cd780bfe6adc476763f43ea22d98c2219
andywan2016 <zw2327@columbia.edu>
2018-12-14 15:30:06 +0000

    add file

---
+++ src/xmlPaser.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+
+public class xmlPaser{
+	public String xmlfile;
+	xmlPaser(String xmlfile){
+	 this.xmlfile=xmlfile;
+	}
+	
+	public int getNumOfColumns(){
+
+	return 0;
+	}
+
+}
