1684763ecc63c48279c933aff90a6b82d24c903e
andy <zw2327@columbia.edu>
2018-12-14 08:40:55 -0500

    Create tree.py

---
+++ pythonTest/tree.py
@@ -0,0 +1,146 @@
+class Node:
+    def __init__(self,val):
+        self.val=val
+        self.left=None
+        self.right=None
+
+
+def dfs_re1(root):
+    if not root:
+        return 
+    else:
+        print(root.val)
+        dfs_re1(root.left)
+        dfs_re1(root.right)
+        
+def dfs_re2(root):
+    if not root:
+        return 
+    else:
+        
+        dfs_re2(root.left)
+        print(root.val)
+        dfs_re2(root.right)
+        
+        
+def dfs_re3(root):
+    if not root:
+        return 
+    else:
+        
+        dfs_re3(root.left)
+        dfs_re3(root.right)
+        print(root.val)
+
+
+def dfs_stack1(root):
+    stackList=[root]   
+    left_visited=set()
+    right_visited=set()
+
+    while stackList:    
+        root=stackList[-1]
+        if root is None:
+            stackList.pop()
+            #root=stackList[-1]
+            '''
+            root=stackList[-1]
+            stackList.append(root.right)
+            root=root.right
+            '''
+        else:           
+            if root not in right_visited and root not in left_visited:                
+                print(root.val)
+                stackList.append(root.left)
+                left_visited.add(root)
+                #root=root.left
+            
+            elif root not in right_visited and root in left_visited:
+                stackList.append(root.right)
+                right_visited.add(root)
+                #root=root.right
+            
+            
+            elif root in right_visited and root in left_visited:
+                stackList.pop()
+                #if 
+                #root=stackList[-1]
+            
+            
+            else:
+                raise ValueError
+                
+                
+def dfs_stack2(root):
+    stackList=[root]   
+    left_visited=set()
+    right_visited=set()
+
+    while stackList:    
+        root=stackList[-1]
+        if root is None:
+            stackList.pop()
+            #root=stackList[-1]
+            '''
+            root=stackList[-1]
+            stackList.append(root.right)
+            root=root.right
+            '''
+        else:           
+            if root not in right_visited and root not in left_visited:                
+                
+                stackList.append(root.left)
+                left_visited.add(root)
+                #root=root.left
+            
+            elif root not in right_visited and root in left_visited:
+                print(root.val)
+                stackList.append(root.right)
+                right_visited.add(root)
+                #root=root.right
+            
+            
+            elif root in right_visited and root in left_visited:
+                stackList.pop()
+                #if 
+                #root=stackList[-1]
+            
+            
+            else:
+                raise ValueError
+    
+        
+
+
+
+def main():
+    '''
+    Construct Tree
+            A
+           / \
+          B   C
+         /   / \
+        D   E   F
+    '''
+    
+    A=Node('A')
+    B=Node('B')
+    C=Node('C')
+    D=Node('D')
+    E=Node('E')
+    F=Node('F')
+    A.left=B
+    A.right=C
+    B.left=D
+    C.left=E
+    C.right=F
+    dfs_re1(A)
+    print('-------')
+    dfs_stack1(A)
+    print('#########')
+    dfs_re2(A)
+    print('-------')
+    dfs_stack2(A)
+    
+    
+main()
