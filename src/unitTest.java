ef8c21bce1d2ba9044f9f445470237c002a16580
andywan2016 <zw2327@columbia.edu>
2018-12-14 13:34:33 +0000

    update

--- src/unitTest.java
+++ src/unitTest.java
@@ -1,5 +1,6 @@
 public class unitTest{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        xmlPaser test=new xmlPaser("H");
+	System.out.println(test.xmlfile);
     }
 }
